c961a860448937ee6f7983b9a70193ba3c6e6708
Masashi Kondo <masashi.kondo@rokka.dev>
2023-02-11 17:23:30 +0900

    fix table

--- mod.ts
+++ mod.ts
@@ -6,6 +6,26 @@ import {
 
 const isValidDate = (date: Date) => !Number.isNaN(date.getTime());
 
+const extractPropFromUnknownJson = <T>(
+  json: unknown,
+  propName: string,
+  nallowing: (x: unknown) => x is T,
+): T | null => {
+  if (!isString(json)) {
+    return null;
+  }
+  const jsonObject = JSON.parse(json);
+
+  if (
+    !isObject(jsonObject) || !(propName in jsonObject) ||
+    !nallowing(jsonObject[propName])
+  ) {
+    return null;
+  }
+
+  return jsonObject[propName] as T;
+};
+
 function validateCommandLineArgument(input: unknown) {
   if (
     !isObject(input) || !("_" in input) || !Array.isArray(input._)
@@ -13,17 +33,24 @@ function validateCommandLineArgument(input: unknown) {
     throw new Error("invalid argument");
   }
 
+  const body = "body" in input
+    ? extractPropFromUnknownJson(input.body, "body", isString) ?? ""
+    : "";
+  const commits = "commits" in input
+    ? extractPropFromUnknownJson(input.commits, "commits", Array.isArray) ?? []
+    : [];
+  const createdAt = "createdAt" in input
+    ? extractPropFromUnknownJson(input.createdAt, "createdAt", isString) ?? ""
+    : "";
+  const closedAt = "closedAt" in input
+    ? extractPropFromUnknownJson(input.closedAt, "closedAt", isString) ?? ""
+    : "";
+
   return {
-    body: "body" in input && isString(input.body) ? input.body : null,
-    commits: "commits" in input && isString(input.commits)
-      ? JSON.parse(input.commits)
-      : [],
-    createdAt: "createdAt" in input && isValidDate(new Date(input.createdAt))
-      ? new Date(input.createdAt)
-      : null,
-    closedAt: "closedAt" in input && isValidDate(new Date(input.closedAt))
-      ? new Date(input.closedAt)
-      : null,
+    body,
+    commits,
+    createdAt: isValidDate(new Date(createdAt)) ? new Date(createdAt) : null,
+    closedAt: isValidDate(new Date(closedAt)) ? new Date(closedAt) : null,
   };
 }
 
@@ -40,4 +67,38 @@ function commandLineArgument() {
 
 const obj = commandLineArgument();
 
-console.log(`${obj.body}\n${obj.createdAt}\n${obj.commits[0]}`);
+console.log(obj.commits[0]);
+
+const firstCommitDuration = `h`;
+const approvedDuration = `h`;
+const closedDuration = `h`;
+
+const resultBody = `
+| index           | datetime                                                | duration               |
+| --------------- | ------------------------------------------------------- | ---------------------- |
+| PR Opened At:   | ${obj.closedAt?.toISOString()}                          | -                      |
+| First Commit At | ${
+  new Date(obj.commits[0].committedDate).toISOString()
+} | ${firstCommitDuration} |
+| Approved At     | ${
+  new Date(obj.commits[0].committedDate).toISOString()
+} | ${approvedDuration}    |
+| PR Closed At    | ${obj.closedAt?.toISOString()}                          | ${closedDuration}      |
+
+First Reviewer:\t${obj.commits[0].authors[0].name}
+`;
+
+const commentWrapdBody =
+  `<!-- pinata: start -->\n${resultBody}\n<!-- pinata: end -->`;
+
+const regExp = /<!--\s*pinata:\s*start\s*-->(.*?)<!--\s*pinata:\s*end\s*-->/gms;
+
+if (obj.body?.match(regExp)) {
+  const replacedBody = obj.body?.replace(
+    /<!--\s*pinata:\s*start\s*-->(.*?)<!--\s*pinata:\s*end\s*-->/gms,
+    (_, __) => commentWrapdBody,
+  );
+  console.log(`${replacedBody}`);
+} else {
+  console.log(`${obj.body}\n${commentWrapdBody}`);
+}
